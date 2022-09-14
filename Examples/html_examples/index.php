6cd437d06109b0318ff374485c9476b18a41251e
Samer Samaha <sbsamaha@wccnet.edu>
2022-09-13 22:45:31 -0400

    updates

---
+++ Examples/html_examples/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Hello</h1>
+    </header>
+
+    <p>paragraph<p>
+      <!--  <p><h1>Hello World</h1><header></header><p> this would fail-->
+
+    <ul>
+        <li>list item 1</li>
+        <li>list item 2</li>
+        <li>list item 3
+            <ol>
+                <li>sublist item 1</li>
+                <li>sublist item 2</li>
+            </ol>
+        </li>
+        <li>list item 4</li>
+    </ul>
+
+    <p> Visit the <a href="http://www.google.com">Google Search Engine</a><p>
+
+    <table border ="1">
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 2</td>
+            <td>Cell 3</td>
+        </tr>
+
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 2</td>
+            <td>Cell 3</td>
+        </tr>
+        
+        <tr>
+            <td>Cell 1</td>
+            <td>Cell 2</td>
+            <td>Cell 3</td>
+        </tr>
+    </table>
+</body>
+</html>
