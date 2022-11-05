817696fd91bbda2d2d6de260b48c876a6e832866
Samer Samaha <sbsamaha@wccnet.edu>
2022-11-05 10:58:38 -0400

    updates

---
+++ Assignments/Assignment7/index.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'classes/fileUpload.php';
+
+
+$fileUpload = new fileUpload();
+$output = $fileUpload->addFile();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS --> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+    <title>Assignment 7</title>
+</head>
+<body>
+    
+<div id="wrapper" class="container">
+        <h1>File Upload</h1>
+        
+
+    <form method="POST" action="index.php">
+
+
+    <div class ="form-row">
+        <?php echo $output ?>
+    </div>
+
+    <div class="form-row">  
+        <label for="names">File Name</label>
+        <input type="text" class="form-control" name= "fileName" id="fileName">
+    </div>
+
+    <div class="form-row">
+        <input type="file" class="form-group" name="pdf_file" accept=".pdf" title="Upload File"/>
+    </div>
+
+    <div class="form-row">
+        <input type="submit" class="btn btn-primary" name="upload" value="Upload File">
+    </div>
+
+</div>
+
+
+</body>
+</html>
